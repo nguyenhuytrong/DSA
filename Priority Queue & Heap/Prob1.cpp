abfabc38022f5609c9a70228391f23ef7639a466
Trong <nguyenhuytronglqd2007@gmail.com>
2025-07-05 15:12:49 +0700

    update

--- Priority Queue & Heap/Prob1.cpp
+++ Priority Queue & Heap/Prob1.cpp
@@ -22,3 +22,29 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+        for (int i : nums) freq[i]++;
+
+        int rows = nums.size() + 1;
+        vector<list<int>> buckets(rows);
+        for (auto [num, count] : freq) {
+            buckets[count].push_back(num);  // tần suất => index
+        }
+        
+        vector<int> res;
+        for (int i = nums.size(); i >= 0 && res.size() < k; i--) {
+            if (!buckets[i].empty()) {
+                for (int num : buckets[i]) {
+                    res.push_back(num);
+                    if (res.size() == k) break;
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
---
+++ Priority Queue & Heap/readme.md
@@ -0,0 +1,6 @@
+### Priority queue
+
+### Heap
+
+### Problems
+- Prob1: [347. Top K Frequent Elements](https://leetcode.com/problems/top-k-frequent-elements/description/)
