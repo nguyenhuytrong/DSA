3a2ecfb297059cadc1766a9ba65ea0b4b57e8675
Trong <nguyenhuytronglqd2007@gmail.com>
2025-07-07 23:33:29 +0700

    complete prob2

--- Priority Queue & Heap/Prob2.cpp
+++ Priority Queue & Heap/Prob2.cpp
@@ -1,3 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for (int i : nums) {
+            pq.push(i); 
+        }
+
+        for (int i = 0; i < k - 1; i++) {
+            pq.pop();
+        }
+
+        return pq.top();
+    }
+};
