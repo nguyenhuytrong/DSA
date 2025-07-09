6c3af7f10465f98ec5ad66e5ba54515bb120cbe5
Trong <nguyenhuytronglqd2007@gmail.com>
2025-07-09 08:04:50 +0700

    add prob4

---
+++ Priority Queue & Heap/Prob4.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for (int i : stones) pq.push(i);
+        while (pq.size() > 1) {
+            int a = pq.top(); pq.pop();
+            int b = pq.top(); pq.pop();
+            pq.push(a-b);
+        }
+        return pq.top();
+    }
+};
--- Priority Queue & Heap/readme.md
+++ Priority Queue & Heap/readme.md
@@ -6,3 +6,4 @@
 - Prob1: [347. Top K Frequent Elements](https://leetcode.com/problems/top-k-frequent-elements/description/)
 - Prob2: [215. Kth Largest Element in an Array](https://leetcode.com/problems/kth-largest-element-in-an-array/)
 - Prob3: [506. Relative Ranks](https://leetcode.com/problems/relative-ranks/)
+- Prob4: [1046. Last Stone Weight](https://leetcode.com/problems/last-stone-weight/)
