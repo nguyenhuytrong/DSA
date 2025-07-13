16f9d8c92851cbbae1d4ae27571bb23c59b565a5
Trong <nguyenhuytronglqd2007@gmail.com>
2025-07-13 09:46:39 +0700

    update prob1 ll

--- Linked list/Prob1.cpp
+++ Linked list/Prob1.cpp
@@ -1,3 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curN = head;
+        ListNode* temp = nullptr;
+
+        while (curN != nullptr && curN->next != nullptr) {
+            if (curN->val == curN->next->val) {
+                temp = curN->next->next;
+                curN->next = temp;
+            } else {
+                curN = curN->next;
+            }
+        }
+
+        return head;
+    }
+};
